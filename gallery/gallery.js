acd5f91e0b2e1901d422800d9be8cfb2c84d8cad
Makvi-droid <pasbenauro05@gmail.com>
2024-11-09 22:26:48 +0800

    added files

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('#holidayCarousel, #trendingCarousel').on('slid.bs.carousel', function () {
-      console.log('Carousel slide finished');
-      // You can add any custom functionality here, such as animations after slide
+
     });
   });
 
@@ -12,4 +11,71 @@ $(document).ready(function(){
         cartModal.show();
     });
 
+});
+
+$(document).ready(function () {
+    
+  $('form.needs-validation').on('submit', function (event) {
+    
+      if (!this.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+
+     
+      $(this).addClass('was-validated');
+  });
+
+ 
+  $('#validationCustomUsername').on('input', function () {
+      if ($(this).val().trim() === '') {
+          $(this).addClass('is-invalid').removeClass('is-valid');
+      } else {
+          $(this).addClass('is-valid').removeClass('is-invalid');
+      }
+  });
+
+  
+  $('#invalidCheck').on('change', function () {
+      if (!this.checked) {
+          $(this).addClass('is-invalid').removeClass('is-valid');
+      } else {
+          $(this).addClass('is-valid').removeClass('is-invalid');
+      }
+  });
+});
+
+$(document).ready(function () {
+
+  $('form.needs-validation').on('submit', function (event) {
+      let formIsValid = true;
+
+      
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const username = $('#validationTooltipUsername');
+      if (!emailPattern.test(username.val())) {
+          username.addClass('is-invalid').removeClass('is-valid');
+          formIsValid = false;
+      } else {
+          username.addClass('is-valid').removeClass('is-invalid');
+      }
+
+      
+      const password = $('#validationTooltipPassword');
+      if (password.val().trim() === '') {
+          password.addClass('is-invalid').removeClass('is-valid');
+          formIsValid = false;
+      } else {
+          password.addClass('is-valid').removeClass('is-invalid');
+      }
+
+    
+      if (!formIsValid) {
+          event.preventDefault();
+          event.stopPropagation();
+      }
+      
+     
+      $(this).addClass('was-validated');
+  });
 });
