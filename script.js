0e56673eed4efc5626f56f792460c9a6c005d6a3
Makvi-droid <pasbenauro05@gmail.com>
2024-11-23 01:11:08 +0800

    adjusting log in and sign up page

--- script.js
+++ script.js
@@ -1,66 +1,13 @@
 $(document).ready(function () {
-    
-    $('form.needs-validation').on('submit', function (event) {
-      
-        if (!this.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
+  const $sign_in_btn = $('#sign-in-btn');
+  const $sign_up_btn = $('#sign-up-btn');
+  const $container = $('.container');
 
-       
-        $(this).addClass('was-validated');
-    });
+  $sign_up_btn.on('click', function () {
+    $container.addClass('sign-up-mode');
+  });
 
-   
-    $('#validationCustomUsername').on('input', function () {
-        if ($(this).val().trim() === '') {
-            $(this).addClass('is-invalid').removeClass('is-valid');
-        } else {
-            $(this).addClass('is-valid').removeClass('is-invalid');
-        }
-    });
-
-    
-    $('#invalidCheck').on('change', function () {
-        if (!this.checked) {
-            $(this).addClass('is-invalid').removeClass('is-valid');
-        } else {
-            $(this).addClass('is-valid').removeClass('is-invalid');
-        }
-    });
-});
-
-$(document).ready(function () {
-  
-    $('form.needs-validation').on('submit', function (event) {
-        let formIsValid = true;
-
-        
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const username = $('#validationTooltipUsername');
-        if (!emailPattern.test(username.val())) {
-            username.addClass('is-invalid').removeClass('is-valid');
-            formIsValid = false;
-        } else {
-            username.addClass('is-valid').removeClass('is-invalid');
-        }
-
-        
-        const password = $('#validationTooltipPassword');
-        if (password.val().trim() === '') {
-            password.addClass('is-invalid').removeClass('is-valid');
-            formIsValid = false;
-        } else {
-            password.addClass('is-valid').removeClass('is-invalid');
-        }
-
-      
-        if (!formIsValid) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        
-       
-        $(this).addClass('was-validated');
-    });
+  $sign_in_btn.on('click', function () {
+    $container.removeClass('sign-up-mode');
+  });
 });
